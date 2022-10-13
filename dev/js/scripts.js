ffdbebea801a95ecf6379c03ac42ebdea765b95e
katemiraglia <102632409+katemiraglia@users.noreply.github.com>
2022-10-13 01:04:50 -0400

    animation

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -4,21 +4,61 @@ import {
 
 
 
-gsap.from("#first-line", {
-    duration: 1.5,
-    x: 200,
-    alpha: 0
 
-});
-gsap.from("#second-line", {
-    duration: 1.5,
-    x: 200,
-    alpha: 0
+function carheroanimation() {
+    var tl = gsap.timeline();
+    let mm = gsap.matchMedia();
 
-});
-gsap.from("#view", {
-    duration: 1.5,
-    x: -200,
-    alpha: 0
+    mm.add("(min-width: 768px)", () => {
+        // desktop setup code here...
+        tl.from("#first-line", {
+                duration: 1.5,
+                x: 200,
+                alpha: 0
 
-});
+            }, "startani")
+            .from("#second-line", {
+                duration: 1.5,
+                x: 200,
+                alpha: 0
+
+            }, "startani")
+            .from("#view", {
+                duration: 1.5,
+                x: -200,
+                alpha: 0,
+
+            }, "startani");
+    });
+
+    mm.add("(max-width: 767px)", () => {
+        // mobile setup code here...
+        tl.from("#first-line", {
+                duration: 1.5,
+                y: 150,
+                alpha: 0
+
+            }, "startani")
+            .from("#second-line", {
+                duration: 1.5,
+                y: 150,
+                alpha: 0
+
+            }, "startani")
+            .from("#view", {
+                duration: 1.5,
+                y: -150,
+                alpha: 0,
+
+            }, "startani");
+    });
+
+
+    return tl;
+
+}
+
+// carheroanimation();
+
+var mainTL = gsap.timeline();
+mainTL.add(carheroanimation());
