e96818346e1ffc8b96a2620508e5b8a0240cc09a
katemiraglia <102632409+katemiraglia@users.noreply.github.com>
2022-10-19 14:02:53 -0400

    pinkback

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -5,96 +5,16 @@ import {
     ScrollTrigger
 } from "gsap/ScrollTrigger";
 
-
-
-/* import {
-    gsap
-} from "gsap";
-import {
-    ScrollTrigger
-} from "gsap/ScrollTrigger";
-
-/* gsap.to("greenbackground", {
-    x: 100,
-    duration: 3
-
-})
- */
-
-
-/* 
 gsap.registerPlugin(ScrollTrigger);
 
-
-function scrollAnimation() {
-    var tl = gsap.timeline();
-    tl.to("#greenbackground", {
-        duration: 1,
-        x: -400,
-        scrollTrigger: {
-            trigger: "#greenbackground",
-            // toggleActions: "restart pause reverse pause",
-            markers: true,
-            start: "0px 50%",
-            end: "100% 10%",
-            scrub: true,
-            pin: true
-        }
-    });
-    return tl;
-}
-
-
-tl.add(scrollAnimation());
-/* var mainTL = gsap.timeline();
-mainTL.add(scrollAnimation()); */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* import { gsap } from "gsap";
-
-console.log("hello class");
-
-gsap.set(".aqua-box",{transformOrigin:"20% 10%"});
-
-let one = 0.25;
-let halfOfOne = 0.5;
-
-// access gsap | what do you want to animate?
-gsap.to(".aqua-box",{duration:one, rotation:360,backgroundColor:"#fff", scaleX:one, stagger:halfOfOne });
-
- */
-
-// let box = document.querySelector("#aqua-box");
-
-// box.addEventListener("click",function(){
-//     // gsap.to("#aqua-box",{duration:1,x: 100 });
-//     // gsap.to("#aqua-box",{duration:1,x: "+=100" });
-// })
+gsap.to("#dressimage", {
+    duration: 1,
+    x: 5,
+    rotation: 360,
+    scrollTrigger: {
+        trigger: "#pinkbackground",
+        toggleActions: "restart pause reverse pause",
+        markers: true,
+        start:
+    }
+});
