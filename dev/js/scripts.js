3a28decd3650a0bb93693e7b91a35c31094a9a3a
katemiraglia <102632409+katemiraglia@users.noreply.github.com>
2022-10-20 01:21:44 -0400

    animation

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -7,7 +7,7 @@ import {
 
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.to("#dressimage", {
+/* gsap.to("#dressimage", {
     duration: 1,
     x: 5,
     rotation: 360,
@@ -17,16 +17,55 @@ gsap.to("#dressimage", {
         markers: true,
 
     }
-});
+}); */
+gsap.from(".image1", {
+    duration: 2,
+    x: -500,
 
-gsap.to("#pinkbackground", {
-    duration: 1,
-    x: 5,
-    rotation: 360,
     scrollTrigger: {
-        trigger: "#whytext",
+        trigger: ".gallery",
         toggleActions: "restart pause reverse pause",
         markers: true,
+        start: "center center"
+
+
+    }
+});
+gsap.from(".image3", {
+    duration: 2,
+    x: 500,
+
+    scrollTrigger: {
+        trigger: ".gallery",
+        toggleActions: "restart pause reverse pause",
+        markers: true,
+        start: "center center"
+
+
+    }
+});
+
+gsap.from("#pinkbackground", {
+    duration: 1.5,
+    xPercent: -100,
+
+
+    scrollTrigger: {
+        trigger: ".gallery",
+        toggleActions: "restart pause reverse pause",
+        markers: true,
+
+    }
+});
+gsap.from("#second-line", {
+    duration: 1,
+    opacity: 0,
+    scrollTrigger: {
+        trigger: "#dress2",
+        toggleActions: "restart pause reverse pause",
+        color: "rgb(228, 104, 194)",
+        markers: true,
+        start: "top center",
 
     }
 });
