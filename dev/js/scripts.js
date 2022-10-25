acd99b65c82125ce7669b619c4a00656246ff6d8
katemiraglia <102632409+katemiraglia@users.noreply.github.com>
2022-10-25 00:26:08 -0400

    Final

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -20,87 +20,23 @@ function pinkAnimation() {
 function secondlinetext() {
     var tl = gsap.timeline(
         {scrollTrigger: { trigger: "#dressimage", scrub:true, markers: true, start: "center bottom"}});
-    tl.to("#second-line", {duration: 3, color:"#e467c3"});
+    tl.to("#second-line", {duration: 1, color:"#e467c3"});
     return tl;
     }
-    
+function dress2animation() {
+    var tl = gsap.timeline({scrollTrigger: { trigger: "#dressimage", scrub:true, markers: true, start: "top bottom", end:"bottom 83%"}});
+    tl.from("#dress2", {duration: 1, y: 1000});
+   
+    return tl;
+    }
+
+
 var mainTL = gsap.timeline();
 mainTL.add(scrollAnimation())
 .add(pinkAnimation())
-.add(secondlinetext());
+.add(secondlinetext())
+.add(dress2animation());
     
-
-/* this is what i have left to gsap.from("#second-line", {
-    duration: 1,
-    opacity: 0,
-    scrollTrigger: {
-        trigger: "#dress2",
-        toggleActions: "restart pause reverse pause",
-        color: "rgb(228, 104, 194)",
-        markers: true,
-        start: "top center",
-
-/* gsap.to("#dressimage", {
-    duration: 1,
-    x: 5,
-    rotation: 360,
-    scrollTrigger: {
-        trigger: "#pinkbackground",
-        toggleActions: "restart pause reverse pause",
-        markers: true,
-    }
-}); */
-/* gsap.from(".image1", {
-    duration: 2,
-    x: -500,
-
-    scrollTrigger: {
-        trigger: ".gallery",
-        toggleActions: "restart pause reverse pause",
-        markers: true,
-        start: "center center"
-
-
-    }
-});
-gsap.from(".image3", {
-    duration: 2,
-    x: 500,
-
-    scrollTrigger: {
-        trigger: ".gallery",
-        toggleActions: "restart pause reverse pause",
-        markers: true,
-        start: "center center"
-
-
-    }
-});
-
-gsap.from("#pinkbackground", {
-    duration: 1.5,
-    xPercent: -100,
-
-
-    scrollTrigger: {
-        trigger: ".gallery",
-        toggleActions: "restart pause reverse pause",
-        markers: true,
-
-    }
-});
-gsap.from("#second-line", {
-    duration: 1,
-    opacity: 0,
-    scrollTrigger: {
-        trigger: "#dress2",
-        toggleActions: "restart pause reverse pause",
-        color: "rgb(228, 104, 194)",
-        markers: true,
-        start: "top center",
-
-    }
-}); */
 
 
 
