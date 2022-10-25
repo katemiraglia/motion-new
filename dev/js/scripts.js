ba0ec986f54345ff5eb28c963c503447cf481f0f
katemiraglia <102632409+katemiraglia@users.noreply.github.com>
2022-10-24 21:12:04 -0400

    animation

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,11 +1,44 @@
-import {
-    gsap
-} from "gsap";
-import {
-    ScrollTrigger
-} from "gsap/ScrollTrigger";
+
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
+
+function scrollAnimation() {
+    var tl = gsap.timeline({ scrollTrigger: {trigger: ".gallery", scrub:true, markers: true, start: "top 90%", end:"bottom 90%"}});
+    tl.from(".image1", { duration: 2, x: -500 })
+    .from(".image3", { duration: 2, x: 500});
+    return tl;
+}
+
+function pinkAnimation() {
+    var tl = gsap.timeline({scrollTrigger: { trigger: "#pinkbackground", scrub:true, markers: true, start: "top 90%", end:"bottom 95%"}});
+    tl.from("#pinkbackground", {duration: 1.5, xPercent: -100});
+    return tl;
+    }
+
+function secondlinetext() {
+    var tl = gsap.timeline(
+        {scrollTrigger: { trigger: "#dressimage", scrub:true, markers: true, start: "center bottom"}});
+    tl.to("#second-line", {duration: 3, color:"#e467c3"});
+    return tl;
+    }
+    
+var mainTL = gsap.timeline();
+mainTL.add(scrollAnimation())
+.add(pinkAnimation())
+.add(secondlinetext());
+    
+
+/* this is what i have left to gsap.from("#second-line", {
+    duration: 1,
+    opacity: 0,
+    scrollTrigger: {
+        trigger: "#dress2",
+        toggleActions: "restart pause reverse pause",
+        color: "rgb(228, 104, 194)",
+        markers: true,
+        start: "top center",
 
 /* gsap.to("#dressimage", {
     duration: 1,
@@ -15,10 +48,9 @@ gsap.registerPlugin(ScrollTrigger);
         trigger: "#pinkbackground",
         toggleActions: "restart pause reverse pause",
         markers: true,
-
     }
 }); */
-gsap.from(".image1", {
+/* gsap.from(".image1", {
     duration: 2,
     x: -500,
 
@@ -68,4 +100,8 @@ gsap.from("#second-line", {
         start: "top center",
 
     }
-});
+}); */
+
+
+
+
