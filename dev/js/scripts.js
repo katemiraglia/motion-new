daf3787495dfb54abe03ff2fda0c8ff84084d3e8
katemiraglia <102632409+katemiraglia@users.noreply.github.com>
2022-10-26 18:49:45 -0400

    week6

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -5,31 +5,34 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
 function scrollAnimation() {
-    var tl = gsap.timeline({ scrollTrigger: {trigger: ".gallery", scrub:true, markers: true, start: "top 90%", end:"bottom 90%"}});
+    var tl = gsap.timeline({ scrollTrigger: {trigger: ".gallery", scrub:true, start: "top 90%", end:"bottom 90%"}});
     tl.from(".image1", { duration: 2, x: -500 })
     .from(".image3", { duration: 2, x: 500});
     return tl;
 }
 
 function pinkAnimation() {
-    var tl = gsap.timeline({scrollTrigger: { trigger: "#pinkbackground", scrub:true, markers: true, start: "top 90%", end:"bottom 95%"}});
+    var tl = gsap.timeline({scrollTrigger: { trigger: "#pinkbackground", scrub:true, start: "top 90%", end:"bottom 95%"}});
     tl.from("#pinkbackground", {duration: 1.5, xPercent: -100});
     return tl;
     }
 
 function secondlinetext() {
     var tl = gsap.timeline(
-        {scrollTrigger: { trigger: "#dressimage", scrub:true, markers: true, start: "center bottom"}});
+        {scrollTrigger: { trigger: "#dressimage", scrub:true, start: "center bottom"}});
     tl.to("#second-line", {duration: 1, color:"#e467c3"});
     return tl;
     }
 function dress2animation() {
-    var tl = gsap.timeline({scrollTrigger: { trigger: "#dressimage", scrub:true, markers: true, start: "top bottom", end:"bottom 83%"}});
-    tl.from("#dress2", {duration: 1, y: 1000});
-   
+    var tl = gsap.timeline({scrollTrigger: { trigger: "#dressimage", scrub:true, start: "top bottom", end:"bottom 83%"}});
+    tl.from("#dress2", {duration: 1, y: 1000, scale:1.5});
     return tl;
     }
-
+/* function menutext() {
+    var tl = gsap.timeline({scrollTrigger: { trigger: ".image2", scrub:true, markers: true, start: "top top"}});
+    tl.to(".centered", {duration: 3, opacity:0});
+    return tl;
+    } */
 
 var mainTL = gsap.timeline();
 mainTL.add(scrollAnimation())
