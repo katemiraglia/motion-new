209ba3ce2a638639b7d972b91abb1819732e7f51
katemiraglia <102632409+katemiraglia@users.noreply.github.com>
2022-11-02 13:21:12 -0400

    week 7

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,21 +1,19 @@
 import { gsap } from "gsap";
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+import { GSDevTools } from "gsap/GSDevTools";
 
-console.log("hello class");
-
-gsap.set(".aqua-box",{transformOrigin:"20% 10%"});
-
-let one = 0.25;
-let halfOfOne = 0.5;
-
-// access gsap | what do you want to animate?
-gsap.to(".aqua-box",{duration:one, rotation:360,backgroundColor:"#fff", scaleX:one, stagger:halfOfOne });
+gsap.registerPlugin(DrawSVGPlugin, GSDevTools);
 
 
+function drawLines(){
+    var tl = gsap.timeline();
+    tl.fromTo("#square",{drawSVG:"75% 75%"},{duration:2, fill:"white", drawSVG:"25% 125%"},"same")
+    .from("#line",{duration:2, drawSVG:"0%"},"same");
+    return tl;
+}
 
-// let box = document.querySelector("#aqua-box");
+var mainTl = gsap.timeline();
+mainTl.add(drawLines());
 
-// box.addEventListener("click",function(){
-//     // gsap.to("#aqua-box",{duration:1,x: 100 });
-//     // gsap.to("#aqua-box",{duration:1,x: "+=100" });
-// })
 
+GSDevTools.create();
