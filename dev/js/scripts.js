ba79e9ca23d2d427e008feea33b1be1bac1ae3b1
katemiraglia <102632409+katemiraglia@users.noreply.github.com>
2022-11-23 00:39:09 -0500

    Working on path

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -3,16 +3,24 @@ import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
 import { GSDevTools } from "gsap/GSDevTools";
 import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
 import { MotionPathHelper } from "gsap/MotionPathHelper";
+import { MotionPathPlugin } from "gsap/MotionPathPlugin";
 
-gsap.registerPlugin(MorphSVGPlugin, DrawSVGPlugin, GSDevTools, MotionPathHelper);
+gsap.registerPlugin(MorphSVGPlugin, DrawSVGPlugin, GSDevTools, MotionPathHelper, MotionPathPlugin);
+
 
 
 
 
 function bluepath2() {
     var tl = gsap.timeline();
-    tl.from("#smallbluecircle", { duration: 6, motionPath: { path: "#bluepath", align: self } });
+    tl.to("#evensmallerblue", { scale: 48, duration: 1, transformOrigin: "center", motionPath: { path: "#bluepath", align: "#bluepath" } })
+        .to("#yellowsmallsquare", { scale: 6, duration: 1, transformOrigin: "center", motionPath: { path: "#yellowpath", align: "#yellowpath" } })
+        .to("#smallgreencircle", { scale: 6, duration: 1, transformOrigin: "center", motionPath: { path: "#greenpath", align: "#greenpath" } })
+        .to("#smallred", { scale: 6, duration: 1, transformOrigin: "center", motionPath: { path: "#redpath", align: "#redpath" } })
 
+    // .to("#smallred", { scale: 6, duration: 6, motionPath: { path: "#redpath", align: "#redpath" } })
+    // .to("#yellowsmallsquare", { scale: 6, duration: 6, motionPath: { path: "#yellowpath", align: "#yellowpath" } })
+    // .to("#smallgreencircle", { scale: 6, duration: 6, motionPath: { path: "#greenpath", align: "#greenpath" } });
 
     return tl;
 }
