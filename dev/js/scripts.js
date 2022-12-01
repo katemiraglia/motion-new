5ad6d7be95ed78a697e55da3335640fd7dab3c0e
katemiraglia <102632409+katemiraglia@users.noreply.github.com>
2022-11-30 19:12:06 -0500

    new

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -4,8 +4,10 @@ import { GSDevTools } from "gsap/GSDevTools";
 import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
 import { MotionPathHelper } from "gsap/MotionPathHelper";
 import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+import { CustomBounce } from "gsap/CustomBounce";
+import { CustomEase } from "gsap/CustomEase";
 
-gsap.registerPlugin(MorphSVGPlugin, DrawSVGPlugin, GSDevTools, MotionPathHelper, MotionPathPlugin);
+gsap.registerPlugin(MorphSVGPlugin, DrawSVGPlugin, GSDevTools, MotionPathHelper, MotionPathPlugin, CustomBounce, CustomEase);
 
 
 
@@ -13,21 +15,54 @@ gsap.registerPlugin(MorphSVGPlugin, DrawSVGPlugin, GSDevTools, MotionPathHelper,
 
 function bluepath2() {
     var tl = gsap.timeline();
-    tl.to("#evensmallerblue", { scale: 48, duration: 1, transformOrigin: "center", motionPath: { path: "#bluepath", align: "#bluepath" } })
-        .to("#yellowsmallsquare", { scale: 6, duration: 1, transformOrigin: "center", motionPath: { path: "#yellowpath", align: "#yellowpath" } })
-        .to("#smallgreencircle", { scale: 6, duration: 1, transformOrigin: "center", motionPath: { path: "#greenpath", align: "#greenpath" } })
-        .to("#smallred", { scale: 6, duration: 1, transformOrigin: "center", motionPath: { path: "#redpath", align: "#redpath" } })
+    CustomBounce.create("yellowbounce", { strength: 0.7, squash: 3, squashID: "yellowbounce-squash" });
+    tl.from("#yellowchangingsphere", { y: -800, x: 600, ease: "yellowbounce", duration: 6 })
+        .to("#yellowchangingsphere", { scaleY: 0.3, scaleX: 1.5, ease: "yellowbounce-squash", transformOrigin: "bottom", duration: 6 }, 0);
+    // .to("#yellowchangingsphere", { x: 500, delay: -4.5 });
+    // .from("#yellowchangingsphere", { y: 500, duration: 6 });
 
-    // .to("#smallred", { scale: 6, duration: 6, motionPath: { path: "#redpath", align: "#redpath" } })
-    // .to("#yellowsmallsquare", { scale: 6, duration: 6, motionPath: { path: "#yellowpath", align: "#yellowpath" } })
-    // .to("#smallgreencircle", { scale: 6, duration: 6, motionPath: { path: "#greenpath", align: "#greenpath" } });
+    // .to("#yellowchangingsphere", { y: -800, x: 600, ease: "yellowbounce", duration: 6 })
+    // .to("#yellowchangingsphere", { scaleY: 0.3, scaleX: 1.5, ease: "yellowbounce-squash", transformOrigin: "bottom", duration: 6 });
+
+    return tl;
+}
+
+// function backup() {
+//     var tl = gsap.timeline();
+//     CustomBounce.create("yellowbounce", { strength: 0.1, squash: 3, squashID: "yellowbounce-squash", endAtStart: true });
+//     tl.from("#yellowchangingsphere", { y: -800, x: 900, ease: "yellowbounce", duration: 6 })
+//         .to("#yellowchangingsphere", { scaleY: 0.3, scaleX: 1.5, ease: "yellowbounce-squash", transformOrigin: "bottom", duration: 6 }, 0)
+
+
+
+
+//     return tl;
+// }
+function shadow() {
+    var tl = gsap.timeline();
+    tl.from("#shadows", { opacity: 0 })
+        .to("#yellowchangingsphere", { rotate: 360, x: 500, duration: 3, transformOrigin: "center" })
+        .to("#shadows", { rotate: 360, x: 500, duration: 3, delay: -3, transformOrigin: "top" })
+        .from("#curvestroke", { drawSVG: "0.100", duration: 3 })
+        .from("#curve", { opacity: 0 });
+    return tl;
+}
+function pathbounce() {
+    var tl = gsap.timeline();
+    tl.from("#docS", { opacity: 0, duration: .3 })
+        .from("#docS", { duration: 4, transformOrigin: "center", motionPath: { path: "#bouncepath1", align: "#bouncepath1" } });
 
     return tl;
 }
 
 
+
 var mainTL = gsap.timeline();
-mainTL.add(bluepath2());
+mainTL.add(bluepath2())
+    // .add(backup())
+    .add(shadow())
+    .add(pathbounce());
+
 
 
 GSDevTools.create();
