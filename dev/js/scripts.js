aff691856c61b340c6243b5c216dbe4d1462c227
katemiraglia <102632409+katemiraglia@users.noreply.github.com>
2022-12-03 17:11:00 -0500

    neew

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -15,35 +15,25 @@ gsap.registerPlugin(MorphSVGPlugin, DrawSVGPlugin, GSDevTools, MotionPathHelper,
 
 function bluepath2() {
     var tl = gsap.timeline();
-    CustomBounce.create("yellowbounce", { strength: 0.7, squash: 3, squashID: "yellowbounce-squash" });
-    tl.from("#yellowchangingsphere", { y: -800, x: 600, ease: "yellowbounce", duration: 6 })
-        .to("#yellowchangingsphere", { scaleY: 0.3, scaleX: 1.5, ease: "yellowbounce-squash", transformOrigin: "bottom", duration: 6 }, 0);
-    // .to("#yellowchangingsphere", { x: 500, delay: -4.5 });
-    // .from("#yellowchangingsphere", { y: 500, duration: 6 });
+    CustomBounce.create("yellowbounce", { strength: 0.5, squash: 3, squashID: "yellowbounce-squash" });
+    tl.from("#rollingtogether_2", { y: -800, x: 600, ease: "yellowbounce", duration: 2 })
+        .to("#rollingtogether_2", { scaleY: 0.3, scaleX: 1.5, ease: "yellowbounce-squash", transformOrigin: "bottom", duration: 2 }, 0);
 
-    // .to("#yellowchangingsphere", { y: -800, x: 600, ease: "yellowbounce", duration: 6 })
-    // .to("#yellowchangingsphere", { scaleY: 0.3, scaleX: 1.5, ease: "yellowbounce-squash", transformOrigin: "bottom", duration: 6 });
+
 
     return tl;
 }
 
-// function backup() {
-//     var tl = gsap.timeline();
-//     CustomBounce.create("yellowbounce", { strength: 0.1, squash: 3, squashID: "yellowbounce-squash", endAtStart: true });
-//     tl.from("#yellowchangingsphere", { y: -800, x: 900, ease: "yellowbounce", duration: 6 })
-//         .to("#yellowchangingsphere", { scaleY: 0.3, scaleX: 1.5, ease: "yellowbounce-squash", transformOrigin: "bottom", duration: 6 }, 0)
 
 
 
 
-//     return tl;
-// }
+
 function shadow() {
     var tl = gsap.timeline();
-    tl.from("#shadows", { opacity: 0 })
-        .to("#yellowchangingsphere", { rotate: 360, x: 500, duration: 3, transformOrigin: "center" })
-        .to("#shadows", { rotate: 360, x: 500, duration: 3, delay: -3, transformOrigin: "top" })
-        .from("#curvestroke", { drawSVG: "0.100", duration: 3 })
+
+
+    tl.from("#curvestroke", { drawSVG: "0.100", duration: 3 })
         .from("#curve", { opacity: 0 });
     return tl;
 }
