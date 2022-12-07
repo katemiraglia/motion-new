67908ae767576ab7f189b4afdf8dad61261447b7
katemiraglia <102632409+katemiraglia@users.noreply.github.com>
2022-12-07 06:14:46 -0500

    GREAT PLACE

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -5,36 +5,57 @@ import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
 import { MotionPathHelper } from "gsap/MotionPathHelper";
 import { MotionPathPlugin } from "gsap/MotionPathPlugin";
 
+
 gsap.registerPlugin(MorphSVGPlugin, DrawSVGPlugin, GSDevTools, MotionPathHelper, MotionPathPlugin);
 
 
 
 
 
-function bluepath2() {
+function redpath2() {
     var tl = gsap.timeline();
-    tl.to("#evensmallerblue", { scale: 64, duration: 1, transformOrigin: "center", motionPath: { path: "#bluepath", align: "#bluepath" } })
-        .to("#smallestYellow", { scale: 8.5, duration: 1, transformOrigin: "center", motionPath: { path: "#yellowpath", align: "#yellowpath" } })
-        .to("#smallestGreen", { scale: 43, duration: 1, transformOrigin: "center", motionPath: { path: "#greenpath", align: "#greenpath" } })
-        .to("#smallestreds", { scale: 35, duration: 1, transformOrigin: "center", motionPath: { path: "#redpath", align: "#redpath" } })
+    tl.set("#bothclipandshapered", { xPercent: -50, yPercent: -50 })
+        .from("#bothclipandshapered", { duration: 5, scale: .015, transformOrigin: "center", motionPath: { path: "#newredpath", align: "#newredpath", alignOrigin: "center" } })
+        .to("#bothclipandshapered", { duration: 3.5, rotate: 360, transformOrigin: "center", delay: -5 })
+        .to("#clipI", { duration: 2, scaleY: 10, transformOrigin: "center", delay: -1.5 });
 
-    // .to("#smallred", { scale: 6, duration: 6, motionPath: { path: "#redpath", align: "#redpath" } })
-    // .to("#yellowsmallsquare", { scale: 6, duration: 6, motionPath: { path: "#yellowpath", align: "#yellowpath" } })
-    // .to("#smallgreencircle", { scale: 6, duration: 6, motionPath: { path: "#greenpath", align: "#greenpath" } });
+    // .set("#bothclipandshapeyellow", { xPercent: -50, yPercent: -50 })
+    // .from("#bothclipandshapeyellow", { duration: 5, scale: .015, transformOrigin: "center", motionPath: { path: "#newyellowpath", align: "#newyellowpath", alignOrigin: "center" } })
+    // .to("#bothclipandshapeyellow", { duration: 3.5, rotate: 360, transformOrigin: "center", delay: -5 })
+    // .to("#ClipX", { duration: 2, scale: 4, transformOrigin: "center", delay: -1.5 });
 
     return tl;
 }
 
-function changingshape() {
+function yellowpath() {
     var tl = gsap.timeline();
-    tl.to("#smallestGreen", { duration: 3, morphSVG: "#Vector" });
+    tl.set("#bothclipandshapeyellow", { xPercent: -50, yPercent: -50 })
+        .from("#bothclipandshapeyellow", { duration: 5, scale: .015, transformOrigin: "center", motionPath: { path: "#newyellowpath", align: "#newyellowpath", alignOrigin: "center" } })
+        .to("#bothclipandshapeyellow", { duration: 3.5, rotate: 360, transformOrigin: "center", delay: -5 })
+        .to("#ClipX", { duration: 2, scale: 4, transformOrigin: "center", delay: -1.5 });
+
+    // .set("#allgreentogther", { xPercent: -50, yPercent: -50 })
+    // .from("#allgreentogther", { duration: 5, scale: .015, transformOrigin: "center", motionPath: { path: "#greenpath", align: "#greenpath", alignOrigin: "center" } })
+    // .to("#bothclipandshapegreen", { duration: 3.5, rotate: 360, transformOrigin: "center", delay: -5 })
+    // .to("#clipgreen", { duration: 2, scale: 4, transformOrigin: "center", delay: -1.5 })
     return tl;
 }
+function greenpath() {
+    var tl = gsap.timeline();
+    tl.set("#allgreentogther", { xPercent: -50, yPercent: -50 })
+        .from("#allgreentogther", { duration: 5, scale: .015, transformOrigin: "center", motionPath: { path: "#greenpath", align: "#greenpath", alignOrigin: "center" } })
+        .to("#bothclipandshapegreen", { duration: 3.5, rotate: 360, transformOrigin: "center", delay: -5 })
+        .to("#clipgreen", { duration: 2, scale: 4, transformOrigin: "center", delay: -1.5 });
+    return tl;
+}
+
 
 
 var mainTL = gsap.timeline();
-mainTL.add(bluepath2())
-    .add(changingshape());
+mainTL.add(yellowpath(), 0)
+    .add(greenpath(), 0)
+    .add(redpath2(), 0);
 
 
 GSDevTools.create();
+// MotionPathHelper.create("#newredpath");
